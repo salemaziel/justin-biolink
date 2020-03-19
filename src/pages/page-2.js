4bc1b7c08145ec8f43b9633321aaac028e480ba8
salemaziel <mymainemail0501@gmail.com>
2020-03-19 00:52:27 -0700

    first commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from '../components/Layout'
+//import Layout3 from '../components/Layout3'
+import Productpic from '../components/productpic'
+
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle,
+    
+  } from 'reactstrap'
+
+
+const Page2 = () => (
+        <Layout>
+           <Productpic />
+        </Layout>
+)
+
+export default Page2
